e7fdc9fff88c317e1407ae2231ff784e95e8d398
usmonovsanjar <sanjarusmonov231@gmail.com>
2025-09-28 17:54:52 +0500

    fix meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app:{
+    head:{
+      title:"wolf",
+      link:[
+        {rel:"icon",type:"image/png", href:"/wolf.png"},
+      ],
+      meta:[
+          {name:"wolf",content:"this is a wolf"},
+        {name:"keywords",content:"this is a keywords"},
+      ]
+    }
+  }
 })
